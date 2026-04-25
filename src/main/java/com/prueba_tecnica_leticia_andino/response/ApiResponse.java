3892a8da6f32488778c52ad9ee9fbe93ae42f406
leticiaandino <leticiaandino@gmail.com>
2026-04-25 17:20:55 -0300

    chore: agregar estructura base ApiResponse

--- src/main/java/com/prueba_tecnica_leticia_andino/response/ApiResponse.java
+++ src/main/java/com/prueba_tecnica_leticia_andino/response/ApiResponse.java
@@ -1,4 +1,36 @@
 package com.prueba_tecnica_leticia_andino.response;
 
-public class ApiResponse {
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse<T> {
+
+    private T data;
+    private Meta meta;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Meta {
+        private Long total;
+        private Integer page;
+        private Integer totalPages;
+    }
+
+    public static <T> ApiResponse<T> of(T data, Long total, Integer page, Integer limit) {
+        int totalPages = (int) Math.ceil((double) total / limit);
+        return ApiResponse.<T>builder()
+                .data(data)
+                .meta(Meta.builder()
+                        .total(total)
+                        .page(page)
+                        .totalPages(totalPages)
+                        .build())
+                .build();
+    }
 }

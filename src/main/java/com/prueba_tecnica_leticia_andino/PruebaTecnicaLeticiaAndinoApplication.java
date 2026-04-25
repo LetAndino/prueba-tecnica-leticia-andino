84ec599a4b1fd7a2d13e5cbfdfb3d0377418e630
leticiaandino <leticiaandino@gmail.com>
2026-04-25 12:40:42 -0300

    fix: commit inicial

---
+++ src/main/java/com/prueba_tecnica_leticia_andino/PruebaTecnicaLeticiaAndinoApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_tecnica_leticia_andino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaLeticiaAndinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaLeticiaAndinoApplication.class, args);
+	}
+
+}
